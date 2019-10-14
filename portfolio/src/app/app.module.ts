b254526834ad6224d708cf0f94e386b72a9c2e65
AkshaySharma-Akay <akshaysharmaak2@gmail.com>
2019-10-14 19:42:34 +0530

    Initial setup completed, header code posted

---
+++ portfolio/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SiteMainComponent } from './site/site-main/site-main.component';
+import { HeaderComponent } from './site/header/header.component';
+import { FooterComponent } from './site/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PageNotFoundComponent,
+    SiteMainComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
